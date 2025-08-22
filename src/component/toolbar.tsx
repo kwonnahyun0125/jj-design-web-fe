f7646047d7d24cdf595671d2dac864476933ef27
jrkgus413 <jrkgus413@naver.com>
2025-08-22 15:28:36 +0900

    refactor: 툴바에서 인스타그램, 블로그 버튼 업데이트 및 버튼 컴포넌트 적용

--- src/component/toolbar.tsx
+++ src/component/toolbar.tsx
@@ -1,7 +1,8 @@
 "use client";
 import Image from "next/image";
-import { NavBar } from "./navbar";
 import Link from "next/link";
+import { NavBar } from "./navbar";
+import { Button } from "./button";
 
 export const Toolbar = () => {
   return (
@@ -25,32 +26,47 @@ export const Toolbar = () => {
             <NavBar />
             {/* 오른쪽: 버튼 */}
             <div className="hidden sm:flex sm:items-end sm:space-x-4 group">
-              <button
+              {/* 인스타그램 버튼 */}
+              <Button
                 className="text-gray-900 px-2 py-2 text-m font-medium hover:border-b-3 hover:border-gray-900 flex items-center justify-center"
                 style={{ minWidth: "50px", cursor: "pointer" }}
+                onClick={() =>
+                  // window.open(
+                  //   "https://blog.naver.com/jj-design_part2",
+                  //   "_blank"
+                  // )
+                  alert("인스타그램은 준비중입니다.")
+                }
               >
                 <Image
                   className="inline-block"
-                  src="/image/instagram.png"
+                  src="/svg/Instagram.svg"
                   alt="Instagram"
                   width={25}
                   height={25}
                   priority
                 />
-              </button>
-              <button
+              </Button>
+              {/* 블로그 버튼 */}
+              <Button
                 className="text-gray-900 px-2 py-2 text-m font-medium hover:border-b-3 hover:border-gray-900 flex items-center justify-center"
                 style={{ minWidth: "50px", cursor: "pointer" }}
+                onClick={() =>
+                  window.open(
+                    "https://blog.naver.com/jj-design_part2",
+                    "_blank"
+                  )
+                }
               >
                 <Image
                   className="inline-block"
-                  src="/image/blog.png"
+                  src="/svg/blog.svg"
                   alt="Naver Blog"
                   width={25}
                   height={25}
                   priority
                 />
-              </button>
+              </Button>
               {/* 상담 요청 버튼 누르면 상담 요청 페이지로 이동 */}
               <Link
                 href="/advice"
