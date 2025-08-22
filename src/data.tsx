a0a9afb2efaac06e51d10d3bcd7517c679f0d691
jrkgus413 <jrkgus413@naver.com>
2025-08-22 15:18:57 +0900

    feat: mock data 수정

--- src/data.tsx
+++ src/data.tsx
@@ -52,120 +52,117 @@ export const menuItems = [
 ];
 
 export const projectsItems = [
+  /**
+   * public/image/mock/건물1.png 
+   * public/image/mock/건물2.png 
+   * public/image/mock/건물3.png 
+   * public/image/mock/사무실.png 
+   * public/image/mock/카페.png
+   * public/image/mock/학원.png
+   */
   {
     title: "범어청구하이츠 32PY",
     year: 2000,
     price: "49,000,000",
-    img: "/image/mock/건축.png",
+    img: "/image/mock/건물1.png",
     desc: "평당 153만원",
   },
   {
     title: "범어숲 화성파크드림S 34PY",
     year: 2013,
     price: "비공개",
-    img: "/image/mock/상업.png",
+    img: "/image/mock/건물2.png",
     desc: "고객 요청으로 비공개",
   },
   {
     title: "세천한라비발디 33PY",
     year: 2016,
     price: "54,328,000",
-    img: "/image/mock/주방.png",
+    img: "/image/mock/건물3.png",
     desc: "평당 164만원",
   },
   {
     title: "범어청구하이츠 35PY",
     year: 2002,
     price: "52,000,000",
-    img: "/image/mock/화장실.png",
+    img: "/image/mock/사무실.png",
     desc: "평당 150만원",
   },
   {
     title: "범어숲 화성파크드림S 36PY",
     year: 2015,
     price: "60,000,000",
-    img: "/image/mock/건축.png",
+    img: "/image/mock/카페.png",
     desc: "평당 166만원",
   },
   {
     title: "범어청구하이츠 32PY",
     year: 2000,
     price: "49,000,000",
-    img: "/image/mock/건축.png",
+    img: "/image/mock/학원.png",
     desc: "평당 153만원",
   },
   {
     title: "범어숲 화성파크드림S 34PY",
     year: 2013,
     price: "비공개",
-    img: "/image/mock/상업.png",
+    img: "/image/mock/건물1.png",
     desc: "고객 요청으로 비공개",
   },
   {
     title: "세천한라비발디 33PY",
     year: 2016,
     price: "54,328,000",
-    img: "/image/mock/주방.png",
+    img: "/image/mock/건물2.png",
     desc: "평당 164만원",
   },
   {
     title: "범어청구하이츠 35PY",
     year: 2002,
     price: "52,000,000",
-    img: "/image/mock/화장실.png",
+    img: "/image/mock/건물3.png",
     desc: "평당 150만원",
   },
   {
     title: "범어숲 화성파크드림S 36PY",
     year: 2015,
     price: "60,000,000",
-    img: "/image/mock/건축.png",
+    img: "/image/mock/사무실.png",
     desc: "평당 166만원",
   },
   {
     title: "범어청구하이츠 32PY",
     year: 2000,
     price: "49,000,000",
-    img: "/image/mock/건축.png",
+    img: "/image/mock/카페.png",
     desc: "평당 153만원",
   },
   {
     title: "범어숲 화성파크드림S 34PY",
     year: 2013,
     price: "비공개",
-    img: "/image/mock/상업.png",
+    img: "/image/mock/학원.png",
     desc: "고객 요청으로 비공개",
   },
   {
     title: "세천한라비발디 33PY",
     year: 2016,
     price: "54,328,000",
-    img: "/image/mock/주방.png",
+    img: "/image/mock/건물1.png",
     desc: "평당 164만원",
   },
   {
     title: "범어청구하이츠 35PY",
     year: 2002,
     price: "52,000,000",
-    img: "/image/mock/화장실.png",
+    img: "/image/mock/건물2.png",
     desc: "평당 150만원",
   },
   {
     title: "범어숲 화성파크드림S 36PY",
     year: 2015,
     price: "60,000,000",
-    img: "/image/mock/건축.png",
+    img: "/image/mock/건물3.png",
     desc: "평당 166만원",
-  },
-];
-
-export const imageItems = [
-  { title: "건축", description: "건축 이미지", src: "/image/mock/건축.png" },
-  { title: "상업", description: "상업 이미지", src: "/image/mock/상업.png" },
-  { title: "주방", description: "주방 이미지", src: "/image/mock/주방.png" },
-  {
-    title: "화장실",
-    description: "화장실 이미지",
-    src: "/image/mock/화장실.png",
   },
 ];
