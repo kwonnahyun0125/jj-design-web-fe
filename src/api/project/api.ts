f47f8351671a641d643a08f29355165fc858617c
jrkgus413 <jrkgus413@naver.com>
2025-09-04 16:59:03 +0900

    feat: 프로젝트 API 및 데이터 구조 추가, 타입 수정

---
+++ src/api/project/api.ts
@@ -0,0 +1,26 @@
+import { ProjectCondition } from "@/type/project";
+import instance from "../setup";
+
+/**
+ * @descript 프로젝트 목록 조회
+ */
+export const getProjectList = async (query: ProjectCondition) => {
+  const response = await instance.get("/projects", { params: query });
+  return response.data;
+};
+
+/**
+ * @descript 프로젝트 상세 조회
+ */
+export const getProjectById = async (id: number) => {
+  const response = await instance.get(`/projects/${id}`);
+  return response.data;
+};
+
+/**
+ * @descript 프로젝트 이미지 키워드 조회
+ */
+export const getProjectImageKeywords = async (id: number) => {
+  const response = await instance.get(`/projects/${id}/image-keywords`);
+  return response.data;
+};
---
+++ src/api/project/data.ts
@@ -0,0 +1,18 @@
+// 프로젝트 기본형
+export const defaultProject = {
+  id: 0,
+  title: "",
+  imageUrl: "",
+  description: "",
+  areaSize: 0,
+  type: "",
+  durationWeeks: 0,
+  reviews: "",
+}
+
+export const keywordFilters = [
+  { value: "APART", label: "아파트" },
+  { value: "HOUSE", label: "주택" },
+  { value: "COMMERCIAL", label: "상가" },
+  { value: "NEW", label: "신축" },
+];
