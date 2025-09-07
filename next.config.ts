e774233bac6add92e077680f85ff53c78b0737da
jrkgus413 <jrkgus413@naver.com>
2025-09-07 17:42:20 +0900

    feat: next.config.ts에 이미지 원격 패턴 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
+  experimental: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
